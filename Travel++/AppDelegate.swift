a9e0a8edf7c99cf6e64423fbfea3726ecaf74766
Andrew Hatfield <hatfielddr@gmail.com>
2022-02-24 02:02:22 -0500

    removed api key

--- Travel++/AppDelegate.swift
+++ Travel++/AppDelegate.swift
@@ -13,7 +13,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        GMSServices.provideAPIKey("AIzaSyCjnXNSxjMeUAki7TZ5T8L0zOMHBTl_3ME")
+        GMSServices.provideAPIKey("")
         
         return true
     }
