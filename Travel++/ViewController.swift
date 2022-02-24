db312334c37a2d52137401a93ad2e9a2dfb5c01a
Emily Blanchard <emily@4blanchards.com>
2022-02-24 14:11:30 -0500

    Merge Tabcontroller and Login Page (Firebase included but commented out)

--- Travel++/ViewController.swift
+++ Travel++/ViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 import GoogleMaps
-import FirebaseDatabase
+//import FirebaseDatabase
 
 class ViewController: UITabBarController {
 
@@ -16,20 +16,9 @@ class ViewController: UITabBarController {
         // Do any additional setup after loading the view.
         
         // Firebase test queries
-        let ref = Database.database().reference()
-        ref.child("someid/name").setValue("User1")
-        
-        
-        // Create a GMSCameraPosition that tells the map to display the
-        // Indy: 39.7169° N, 86.2956° W
-        //39.71503538310621, -86.29779997696295
-        let camera = GMSCameraPosition.camera(withLatitude: 39.7150, longitude: -86.2978, zoom: 18.0)
-        let mapView = GMSMapView.map(withFrame: self.view.frame, camera: camera)
-        mapView.settings.myLocationButton = true
-        mapView.settings.zoomGestures = true
- 
-        self.view.addSubview(mapView)
-
+//        let ref = Database.database().reference()
+//        ref.child("someid/name").setValue("User1")
+//
     }
 
 
