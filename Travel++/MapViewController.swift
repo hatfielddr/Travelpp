248ebe96c49d798b154898dd530d4673c48490a8
Emily Blanchard <emily@4blanchards.com>
2022-02-25 12:12:14 -0500

    Add SFO GPS coordinates

--- Travel++/MapViewController.swift
+++ Travel++/MapViewController.swift
@@ -12,11 +12,17 @@ class MapViewController: UIViewController {
         
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Indy: 39.7169° N, 86.2956° W
-        //39.71503538310621, -86.29779997696295
+        
+        //Indianaplis
+        let latitude = 39.7150
+        let longitude = -86.2978
+        
+        //San Francisco
+//        let latitude = 37.6213
+//        let longitude = -122.3790
 
         //set up map view
-        let camera = GMSCameraPosition.camera(withLatitude: 39.7150, longitude: -86.2978, zoom: 18.0)
+        let camera = GMSCameraPosition.camera(withLatitude: latitude, longitude: longitude, zoom: 18.0)
         let mapView = GMSMapView.map(withFrame: self.view.frame, camera: camera)
         mapView.settings.myLocationButton = true
         mapView.settings.zoomGestures = true
