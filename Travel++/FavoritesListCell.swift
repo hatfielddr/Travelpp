38a0eeecbc750e54842841eb0a66eae4bfaccc03
Tori <vduguid@purdue.edu>
2022-02-24 20:41:43 -0500

    added favorites changes to new branch

---
+++ Travel++/FavoritesListCell.swift
@@ -0,0 +1,12 @@
+//
+//  FavoritesListCell.swift
+//  Travel++
+//
+//  Created by Tori Duguid on 2/24/22.
+//
+
+import UIKit
+
+class FavoriteListCell: UITableViewCell {
+    
+}
---
+++ Travel++/FavoritesListViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FavoritesListViewController.swift
+//  Travel++
+//
+//  Created by Tori Duguid on 2/24/22.
+//
+
+import UIKit
+import SwiftUI
+import FirebaseDatabase
+
+class FavoritesListViewController: UITableViewController {
+    override func viewDidLoad() {
+        // Here we replace the default values
+        getData()
+    }
+    
+    func getData() {
+    }
+}
