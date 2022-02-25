c34775acb47d5db96eab94725cdb0848950d50e8
Tori <vduguid@purdue.edu>
2022-02-24 20:47:53 -0500

    changed api key (again)

--- Travel++/AppDelegate.swift
+++ Travel++/AppDelegate.swift
@@ -14,7 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        GMSServices.provideAPIKey("AIzaSyCjnXNSxjMeUAki7TZ5T8L0zOMHBTl_3ME")
+        GMSServices.provideAPIKey("AIzaSyCTPAwUf5otRc0iasTbKZDY5-jh5HXBxUw")
         FirebaseApp.configure()
         
         return true
