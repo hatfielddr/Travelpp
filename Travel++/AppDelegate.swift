95e7ad331d6cded4bd002768625629b074c3f2c5
emlyblnchrd <43043228+emlyblnchrd@users.noreply.github.com>
2022-04-01 08:05:51 -0400

    emily branch + main

--- Travel++/AppDelegate.swift
+++ Travel++/AppDelegate.swift
@@ -15,8 +15,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        GMSServices.provideAPIKey("AIzaSyCTPAwUf5otRc0iasTbKZDY5-jh5HXBxUw")
-        GMSPlacesClient.provideAPIKey("AIzaSyA7og50VFTk6EC9H-cq09vQk5xGA2znJJc")
+        GMSServices.provideAPIKey("")
+        GMSPlacesClient.provideAPIKey("")
         FirebaseApp.configure()
         
         return true
